e2f6634fa2a075a35397f7228b6cc37731a5dc9f
Gabriel Lima <gabriellimadxgg@gmail.com>
2025-01-18 01:05:32 -0300

    custom botoes

--- main.py
+++ main.py
@@ -22,13 +22,66 @@ class AreaScreen(Screen):
 class ComprimentoScreen(Screen):
     pass
 
+class DensidadeScreen(Screen):
+    pass
+
+
+class DigitalScreen(Screen):
+    pass
+
+
+class EnergiaScreen(Screen):
+    pass
+
+
+class ForcaScreen(Screen):
+    pass
+
+
+class MoedaScreen(Screen):
+    pass
+
+
+class PesoScreen(Screen):
+    pass
+
+
+class PressaoScreen(Screen):
+    pass
+
+
+class TemperaturaScreen(Screen):
+    pass
+
+
+class TempoScreen(Screen):
+    pass
+
+
+class VelocidadeScreen(Screen):
+    pass
+
+
+class VolumeScreen(Screen):
+    pass
+
 
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(AnguloScreen(name='angulo'))
 sm.add_widget(AreaScreen(name='area'))
 sm.add_widget(ComprimentoScreen(name='comprimento'))
-
+sm.add_widget(DensidadeScreen(name='densidade'))
+sm.add_widget(DigitalScreen(name='digital'))
+sm.add_widget(EnergiaScreen(name='energia'))
+sm.add_widget(ForcaScreen(name='forca'))
+sm.add_widget(MoedaScreen(name='moeda'))
+sm.add_widget(PesoScreen(name='peso'))
+sm.add_widget(PressaoScreen(name='pressao'))
+sm.add_widget(TemperaturaScreen(name='temperatura'))
+sm.add_widget(TempoScreen(name='tempo'))
+sm.add_widget(VelocidadeScreen(name='velocidade'))
+sm.add_widget(VolumeScreen(name='volume'))
 
 class HighMetric(MDApp):
 
@@ -39,8 +92,10 @@ class HighMetric(MDApp):
     def toggle_theme(self):
         """Alterna entre o tema claro e escuro."""
         if self.theme_cls.theme_style == "Light":
-            self.theme_cls.theme_style = "Dark"  # Muda para o tema escuro
+            self.theme_cls.primary_palette = "Orange"
+            self.theme_cls.theme_style = "Dark"
         else:
+            self.theme_cls.primary_palette = "Blue"
             self.theme_cls.theme_style = "Light"
 
 
--- tela.py
+++ tela.py
@@ -4,16 +4,28 @@ ScreenManager:
     AnguloScreen:
     AreaScreen:
     ComprimentoScreen:
+    DensidadeScreen:
+    DigitalScreen:
+    EnergiaScreen:
+    ForcaScreen:
+    MoedaScreen:
+    PesoScreen:
+    PressaoScreen:
+    TemperaturaScreen:
+    TempoScreen:
+    VelocidadeScreen:
+    VolumeScreen:
 
 <MenuScreen>:
     name: 'menu'
     MDScreen:
-        md_bg_color: self.theme_cls.bg_light
-
+        md_bg_color:
+        
     MDTopAppBar:
         title: "HighMetric"
         center_title: True
-        left_action_items: [["menu", lambda x: app.toggle_theme()]]
+        left_action_items: [["information-outline", lambda x: print("Informações clicadas")]]
+        right_action_items: [["lightbulb", lambda x: app.toggle_theme()]]
 
     MDIconButton:
         icon: 'imgs/angulo_icon.png'
@@ -23,6 +35,7 @@ ScreenManager:
         on_press: root.manager.current = 'angulo'
         canvas.before:
             Color:
+                rgba: 0.66, 0.66, 0.66, 1
 
             RoundedRectangle:
                 pos: self.pos
@@ -37,7 +50,7 @@ ScreenManager:
         on_press: root.manager.current = 'comprimento'
         canvas.before:
             Color:
-
+                rgba: 0.66, 0.66, 0.66, 1
             RoundedRectangle:
                 pos: self.pos
                 size: self.size
@@ -51,17 +64,173 @@ ScreenManager:
         on_press: root.manager.current = 'area'
         canvas.before:
             Color:
-
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+                
+    MDIconButton:
+        icon: 'imgs/densidade_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.2, 'center_y': 0.8}
+        on_press: root.manager.current = 'densidade'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
             RoundedRectangle:
                 pos: self.pos
                 size: self.size
                 radius: [12]
 
+    MDIconButton:
+        icon: 'imgs/digital_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.8}
+        on_press: root.manager.current = 'digital'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/energia_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.8, 'center_y': 0.8}
+        on_press: root.manager.current = 'energia'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/forca_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.2, 'center_y': 0.7}
+        on_press: root.manager.current = 'forca'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/moeda_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.7}
+        on_press: root.manager.current = 'moeda'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/peso_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.8, 'center_y': 0.7}
+        on_press: root.manager.current = 'peso'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/pressao_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.2, 'center_y': 0.6}
+        on_press: root.manager.current = 'pressao'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/temperatura_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.6}
+        on_press: root.manager.current = 'temperatura'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/tempo_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.8, 'center_y': 0.6}
+        on_press: root.manager.current = 'tempo'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/velocidade_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.2, 'center_y': 0.5}
+        on_press: root.manager.current = 'velocidade'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+    MDIconButton:
+        icon: 'imgs/volume_icon.png'
+        size_hint: None, None
+        size: "128dp", "128dp"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        on_press: root.manager.current = 'volume'
+        canvas.before:
+            Color:
+                rgba: 0.66, 0.66, 0.66, 1
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+                radius: [12]
+
+
+
 <AnguloScreen>:
     name: 'angulo'
     MDScreen:
         MDLabel:
-            text: 'Tengolengotengo'
+            text: 'ANGULO'
             halign: 'center'
 
         MDRectangleFlatButton:
@@ -73,7 +242,7 @@ ScreenManager:
     name: 'area'
     MDScreen:
         MDLabel:
-            text: 'Tengolengotengo'
+            text: 'AREA'
             halign: 'center'
 
         MDRectangleFlatButton:
@@ -86,7 +255,7 @@ ScreenManager:
     name: 'comprimento'
     MDScreen:
         MDLabel:
-            text: 'Tengolengotengo'
+            text: 'COMPRIMENTO'
             halign: 'center'
 
         MDRectangleFlatButton:
@@ -94,4 +263,135 @@ ScreenManager:
             pos_hint: {'center_x': 0.5, 'center_y': 0.1}
             on_press: root.manager.current = 'menu'
 
+<DensidadeScreen>:
+    name: 'densidade'
+    MDScreen:
+        MDLabel:
+            text: 'DENSIDADE'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<DigitalScreen>:
+    name: 'digital'
+    MDScreen:
+        MDLabel:
+            text: 'DIGITAL'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<EnergiaScreen>:
+    name: 'energia'
+    MDScreen:
+        MDLabel:
+            text: 'ENERGIA'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<ForcaScreen>:
+    name: 'forca'
+    MDScreen:
+        MDLabel:
+            text: 'FORÇA'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<MoedaScreen>:
+    name: 'moeda'
+    MDScreen:
+        MDLabel:
+            text: 'MOEDA'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<PesoScreen>:
+    name: 'peso'
+    MDScreen:
+        MDLabel:
+            text: 'PESO'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<PressaoScreen>:
+    name: 'pressao'
+    MDScreen:
+        MDLabel:
+            text: 'PRESSÃO'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<TemperaturaScreen>:
+    name: 'temperatura'
+    MDScreen:
+        MDLabel:
+            text: 'TEMPERATURA'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<TempoScreen>:
+    name: 'tempo'
+    MDScreen:
+        MDLabel:
+            text: 'TEMPO'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<VelocidadeScreen>:
+    name: 'velocidade'
+    MDScreen:
+        MDLabel:
+            text: 'VELOCIDADE'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
+
+<VolumeScreen>:
+    name: 'volume'
+    MDScreen:
+        MDLabel:
+            text: 'VOLUME'
+            halign: 'center'
+
+        MDRectangleFlatButton:
+            text: 'Back'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
+            on_press: root.manager.current = 'menu'
 """
